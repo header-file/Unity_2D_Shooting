4e307673891fa76da57049a6f2ee07743867ff74
header-file <34476408+header-file@users.noreply.github.com>
2020-11-29 16:27:22 +0900

    해상도 조절
    Turrets 움직임 추가
    해상도 조절
    그에 따른 UI 조절

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,6 +6,7 @@ public class UIManager : MonoBehaviour
 {
     public GameObject Player;
     public GameObject SubWeapon;
+    public GameObject Turret;
     public GameObject Background;
     public GameObject Panel;
     public GameObject Slot;
@@ -20,11 +21,13 @@ public class UIManager : MonoBehaviour
 
     Vector3 PlayerPosOrigin;
     Vector3 SubWeaponPosOrigin;
+    Vector3 TurretPosOrigin;
     Vector3 BackgroudnPosOrigin;
     Vector3 PanelPosOrigin;
 
     Vector3 PlayerPosUI;
     Vector3 SubWeaponPosUI;
+    Vector3 TurretPosUI;
     Vector3 BackgroundPosUI;
     Vector3 PanelPosUI;
 
@@ -39,14 +42,16 @@ public class UIManager : MonoBehaviour
     {
         PlayerPosOrigin = Player.transform.position;
         SubWeaponPosOrigin = SubWeapon.transform.position;
+        TurretPosOrigin = Turret.transform.position;
         BackgroudnPosOrigin = Background.transform.position;
         PanelPosOrigin = Panel.transform.position;
-        Debug.Log(PanelPosOrigin);
+        Debug.Log(TurretPosOrigin);
 
-        PlayerPosUI = new Vector3(0.0f, 5.8f, 0.0f);
-        SubWeaponPosUI = new Vector3(0.0f, PlayerPosUI.y - 0.3f, 0.0f);
+        PlayerPosUI = new Vector3(0.0f, 3.5f, 0.0f);
+        SubWeaponPosUI = new Vector3(0.0f, PlayerPosUI.y - 0.24f, 0.0f);
+        TurretPosUI = new Vector3(0.0f, 6.3f, 90.0f);
         BackgroundPosUI = new Vector3(0.0f, PlayerPosUI.y + 3.3f, 0.0f);
-        PanelPosUI = new Vector3(0.0f, 3.8f, 90.0f);
+        PanelPosUI = new Vector3(0.0f, 1.5f, 90.0f);
 
         TickCount = 1.0f / 6.0f;
 
@@ -74,6 +79,7 @@ public class UIManager : MonoBehaviour
     {
         Player.transform.position = Vector3.MoveTowards(Player.transform.position, PlayerPosUI, TickCount);
         SubWeapon.transform.position = Vector3.MoveTowards(SubWeapon.transform.position, SubWeaponPosUI, TickCount);
+        Turret.transform.position = Vector3.MoveTowards(Turret.transform.position, TurretPosUI, TickCount);
         Background.transform.position = Vector3.MoveTowards(Background.transform.position, BackgroundPosUI, TickCount);
         Panel.transform.position = Vector3.MoveTowards(Panel.transform.position, PanelPosUI, TickCount);
 
