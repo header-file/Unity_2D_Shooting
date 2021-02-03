353c6ca348446cef742e71f7b10918e3d845fc45
header-file <34476408+header-file@users.noreply.github.com>
2021-02-03 16:48:22 +0900

    UI 수정

--- Assets/Scripts/UI/PlanetSlot.cs
+++ Assets/Scripts/UI/PlanetSlot.cs
@@ -7,9 +7,22 @@ public class PlanetSlot : MonoBehaviour
 {
     public Image PlanetImage;
     public Text Name;
+    public GameObject Lock;
 
     void Start()
     {
         Name.gameObject.SetActive(false);
+        Lock.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Name.gameObject.activeSelf)
+        {
+            Vector3 pos = PlanetImage.gameObject.GetComponent<RectTransform>().anchoredPosition;
+            pos.y -= 200.0f * PlanetImage.transform.localScale.x;
+            Name.gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
+        }
+            
     }
 }
--- Assets/Scripts/UI/Summary.cs
+++ Assets/Scripts/UI/Summary.cs
@@ -26,27 +26,29 @@ public class Summary : MonoBehaviour
 
     void SummaryOn()
     {
-        RectT.transform.position = Vector3.Lerp(RectT.transform.position, Vector3.zero, Time.deltaTime * 10.0f);
+        RectT.anchoredPosition = Vector3.Lerp(RectT.anchoredPosition, Vector3.zero, Time.deltaTime * 10.0f);
 
-        if (Time.deltaTime > 0.1f)
+        if (Time.deltaTime >= 0.1f)
             IsSummaryOn = false;
     }
 
     void SummaryOff()
     {
-        RectT.transform.position = Vector3.Lerp(RectT.transform.position, OriginalPos, Time.deltaTime * 10.0f);
+        RectT.anchoredPosition = Vector3.Lerp(RectT.anchoredPosition, OriginalPos, Time.deltaTime * 10.0f);
 
-        if (Time.deltaTime > 0.1f)
+        if (Time.deltaTime >= 0.1f)
             IsSummaryOff = false;
     }
 
     public void OnClickSummaryOn()
     {
+        IsSummaryOff = false;
         IsSummaryOn = true;
     }
 
     public void OnClickSummaryOff()
     {
+        IsSummaryOn = false;
         IsSummaryOff = true;
     }
 }
