f625279a2012428fc78ca10609f29a6abbb7006a
header-file <34476408+header-file@users.noreply.github.com>
2021-03-08 17:04:52 +0900

    로컬 세이브 수정 중

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -6,11 +6,41 @@ using System;
 [Serializable]
 public class GameData
 {
+    public struct InventoryData
+    {
+        int Type;
+        int Grade;
+        int Value;
+    }
+
     public int Coin;
+    public int[] Resources;
+    public int[,] SubWeaponLevels;
+    public UpgradeManager.BulletData[] Weapons;
+    public int[,,] Equippeds;
+    public InventoryData[] Inventories;
+
+    public int[] BossGauges;
+
+    void Start()
+    {
+        Resources = new int[StageManager.MAXSTAGES];
+        SubWeaponLevels = new int[StageManager.MAXSTAGES, 4];
+        Weapons = new UpgradeManager.BulletData[Bullet.MAXBULLETS];
+        Equippeds = new int[StageManager.MAXSTAGES, Bullet.MAXBULLETS, 3];
+        Inventories = new InventoryData[GameManager.Inst().Player.MAXINVENTORY];
+
+        BossGauges = new int[StageManager.MAXSTAGES];
+    }
 
     public void SaveData()
     {
         Coin = GameManager.Inst().Player.GetCoin();
+        for(int i = 0; i < StageManager.MAXSTAGES; i++)
+        {
+            Resources[i] = GameManager.Inst().Resources[i];
+            
+        }
     }
 
     public void LoadData()
