72adc4a7cd68116c85a66837afd2dccd111aa957
header-file <34476408+header-file@users.noreply.github.com>
2021-07-30 19:00:03 +0900

    1. Explosion 수정

--- Assets/Scripts/Bullets/Explosion.cs
+++ Assets/Scripts/Bullets/Explosion.cs
@@ -17,16 +17,6 @@ public class Explosion : Bullet
     {
         if(collision.tag == "Enemy")
         {
-            //for (int i = 0; i < ExplodeArea.HitObjects.Length; i++)
-            //{
-            //    Enemy enemy = ExplodeArea.HitObjects[i].GetComponent<Enemy>();
-            //    if (enemy)
-            //    {
-
-            //        enemy.OnHit(, false)
-            //    }
-            //}
-
             Explode.SetActive(true);
             Invoke("Disappear", 0.15f);
         }
