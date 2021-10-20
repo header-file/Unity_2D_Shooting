491d6325f25f6f128fd5febab663321314f6cf7e
header-file <34476408+header-file@users.noreply.github.com>
2021-10-20 17:18:37 +0900

    1. 사운드 추가 2. 시작 화면 사운드 출력

---
+++ Assets/Scripts/UI/MainScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScene : MonoBehaviour
+{
+    public AudioClip MainTheme;
+    public AudioSource BGM;
+
+    void Start()
+    {
+        BGM.clip = MainTheme;
+        BGM.loop = true;
+        BGM.Play();
+    }
+}
