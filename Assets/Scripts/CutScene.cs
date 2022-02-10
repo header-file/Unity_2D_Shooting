86c5ca037f090ac96e10298af879bfc9522fef8b
header-file <34476408+header-file@users.noreply.github.com>
2022-02-10 17:44:50 +0900

    컷씬 동작

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour
+{
+    public Animator Anim;
+    public int MaxCount;
+
+    int SceneCount;
+
+
+    void Start()
+    {
+        SceneCount = 0;
+        gameObject.SetActive(false);
+    }
+
+    void NextScene()
+    {
+        Invoke("ToNext", 1.0f);
+    }
+
+    public void ToNext()
+    {
+        SceneCount++;
+
+        if (SceneCount <= MaxCount)
+            Anim.SetTrigger("Scene" + SceneCount.ToString());
+        else
+            Anim.SetTrigger("End");
+    }
+
+    void End()
+    {
+        gameObject.SetActive(false);
+    }
+}
